2bde1b5e8d0657d6c560b76239dba4b7c82e4959
JoedewYT <joeveraart@gmail.com>
2023-01-04 18:42:26 +0100

    Moved the org files into the dotfiles folder

--- init.el
+++ init.el
@@ -11,9 +11,10 @@
 
 ;; Appearance
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 150)
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 150)
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 150 :weight 'regular)
 ;; Theme is loaded in the use-package init for doom-themes
 (set-frame-parameter (selected-frame) 'alpha '(95 . 95))
-(hl-line-mode 1)
 
 ;; Line numbers
 (column-number-mode)
@@ -120,7 +121,12 @@
 
   (jvec/leader-keys
     "t"  '(:ignore t :which-key "Appearance")
-    "tt" '(counsel-load-theme :which-key "Choose theme")))
+    "tt" '(counsel-load-theme :which-key "Choose theme")
+    "b" '(:ignore t :which-key "Buffer")
+    "bs" '(counsel-switch-buffer :which-key "Switch buffer")
+    "bk" '(kill-buffer :which-key "Kill Buffer")))
+  
+  
 
 (use-package evil
   :init
@@ -164,6 +170,154 @@
   "m" '(:ignore t :which-key "Magit")
   "ms" '(magit-status :which-key "Status"))
 
+(use-package forge)
+
+(defun jvec/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
+(use-package org
+  :hook (org-mode . jvec/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t)
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+  (setq org-agenda-files
+	'("~/.config/emacs/Org-Docs/Tasks.org"
+	  ;;"~/Documents/Org-Docs/Habits.org" (needs to be created)
+	  "~/.config/emacs/Org-Docs/Birthdays.org"))
+
+;;  (require 'org-habit)
+;;  (add-to-list 'org-modules `org-habit)
+;;  (setq org-habit-graph-column 60)
+  
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "NEXT(N)" "|" "DONE(d!)")
+	  (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+
+  (setq org-refile-targets
+        '(("~/.config/emacs/Org-Docs/Archive.org" :maxlevel . 1)
+	  ("~/.config/emacs/Org-Docs/Tasks.org" :maxlevel . 1)))
+
+  ;; Save Org buffers after refiling!
+  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+
+  ;; Configure custom agenda views
+  (setq org-agenda-custom-commands
+   '(("d" "Dashboard"
+     ((agenda "" ((org-deadline-warning-days 7)))
+      (todo "NEXT"
+        ((org-agenda-overriding-header "Next Tasks")))
+      (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+
+    ("n" "Next Tasks"
+     ((todo "NEXT"
+        ((org-agenda-overriding-header "Next Tasks")))))
+
+    ("H" "Homework" tags-todo "+hw")
+
+    ;; Low-effort next actions
+    ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+     ((org-agenda-overriding-header "Low Effort Tasks")
+      (org-agenda-max-todos 20)
+      (org-agenda-files org-agenda-files)))
+
+    ("w" "Workflow Status"
+     ((todo "WAIT"
+            ((org-agenda-overriding-header "Waiting on External")
+             (org-agenda-files org-agenda-files)))
+      (todo "REVIEW"
+            ((org-agenda-overriding-header "In Review")
+             (org-agenda-files org-agenda-files)))
+      (todo "PLAN"
+            ((org-agenda-overriding-header "In Planning")
+             (org-agenda-todo-list-sublevels nil)
+             (org-agenda-files org-agenda-files)))
+      (todo "BACKLOG"
+            ((org-agenda-overriding-header "Project Backlog")
+             (org-agenda-todo-list-sublevels nil)
+             (org-agenda-files org-agenda-files)))
+      (todo "READY"
+            ((org-agenda-overriding-header "Ready for Work")
+             (org-agenda-files org-agenda-files)))
+      (todo "ACTIVE"
+            ((org-agenda-overriding-header "Active Projects")
+             (org-agenda-files org-agenda-files)))
+      (todo "COMPLETED"
+            ((org-agenda-overriding-header "Completed Projects")
+             (org-agenda-files org-agenda-files)))
+      (todo "CANC"
+            ((org-agenda-overriding-header "Cancelled Projects")
+             (org-agenda-files org-agenda-files))))))))
+
+;; Capture Template (not interested for now)
+;;  (setq org-capture-templates
+;;    `(("t" "Tasks / Projects")
+;;      ("tt" "Task" entry (file+olp "~/Documents/Org-Docs/Tasks.org" "Inbox")
+;;           "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
+
+(jvec/leader-keys
+  "o" '(:ignore t :which-key "Org-mode")
+  "oa" '(org-agenda :which-key "Agenda")
+  "os" '(org-schedule :which-key "Schedule")
+  "od" '(org-deadline :which-key "Attach deadline")
+  "ot" '(org-time-stamp :which-key "Attach time stamp")
+  "og" '(counsel-org-tag :which-key "Attach tag")
+  "oe" '(org-set-effort :which-key "Attach effort (time mins)")
+  "or" '(org-refile :which-key "Refile List")
+  "oT" '(lambda () (interactive) (find-file "~/.config/emacs/Org-Docs/Tasks.org") :which-key "Open tasks file")
+  "oB" '(lambda () (interactive) (find-file "~/.config/Org-Docs/Birthdays.org") :which-key "Open birthdays file"))
+
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun jvec/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•")))))))
+
+(with-eval-after-load 'org-faces
+;; Increase the size of various headings
+(set-face-attribute 'org-document-title nil :font "Cantarell" :weight 'bold :height 1.3)
+(dolist (face '((org-level-1 . 1.2)
+                (org-level-2 . 1.1)
+                (org-level-3 . 1.05)
+                (org-level-4 . 1.0)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+;; Ensure that anything that should be fixed-pitch in Org files appears that way
+(set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch))
+
+(defun jvec/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . jvec/org-mode-visual-fill))
+
 
 ;; Install programs unless already installed
 ;;(setq my-packages
@@ -184,7 +338,7 @@
  '(custom-safe-themes
    '("467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" "49acd691c89118c0768c4fb9a333af33e3d2dca48e6f79787478757071d64e68" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
  '(package-selected-packages
-   '(evil-magit magit counsel-projectile projectile hydra evil-collection general doom-themes ivy-rich which-key rainbow-delimiters use-package doom-modeline counsel all-the-icons)))
+   '(org-bullets forge evil-magit magit counsel-projectile projectile hydra evil-collection general doom-themes ivy-rich which-key rainbow-delimiters use-package doom-modeline counsel all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
